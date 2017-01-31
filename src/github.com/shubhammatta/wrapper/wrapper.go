2965b48858707845500b60627cc242bb0eaa3a13
Shubham Matta <cs14btech11032@iith.ac.in>
2017-01-31 16:40:06 +0530

    Wrapper 1.0

--- src/github.com/shubhammatta/wrapper/wrapper.go
+++ src/github.com/shubhammatta/wrapper/wrapper.go
@@ -2,8 +2,27 @@ package main
 
 import (
       "fmt"
+      "os"
+      "time"
 )
 
+type file struct{
+    file_name            string
+    file_path           string
+    number              int
+    result              int8
+    username            string
+    timestamp           time.Time
+    currTestcase        int
+    language            string
+}
+
+
+
+
+
 func main(){
-      fmt.Println("Main commit");
+    arguments := os.Args
+    fmt.Println(arguments)
+        fmt.Println("Main commit");
 }
