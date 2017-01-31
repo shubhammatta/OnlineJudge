333a815991564e46134371a2516295e8c781515c
Shubham Matta <cs14btech11032@iith.ac.in>
2017-02-01 02:35:19 +0530

    Language_detect done

--- src/github.com/shubhammatta/wrapper/wrapper.go
+++ src/github.com/shubhammatta/wrapper/wrapper.go
@@ -4,6 +4,7 @@ import (
       "fmt"
       "os"
       "time"
+      "strings"
 )
 
 type file struct{
@@ -17,12 +18,34 @@ type file struct{
     language            string
 }
 
-
+//0 - C ,  1 - C++ , 2 -  Java  , 3 - Python
+func detect_lang(s string) int{
+    arr := strings.Split(s , ".")
+    n := len(arr)
+    if arr[n-1] == "cpp" || arr[n-1] == "CPP" {
+        return 1
+    }
+    if arr[n-1]=="c" || arr[n-1] == "C"{
+        return 0
+    }
+    if arr[n-1] == "java"{
+        return 2
+    }
+    if arr[n-1] == "py" {
+        return 3
+    }
+    return -1
+}
 
 
 
 func main(){
     arguments := os.Args
     fmt.Println(arguments)
+    f := file{}
+    f.file_name = os.Args[1]
+    f.file_path = os.Args[2]
+    fmt.Println(detect_lang(f.file_name))
+
         fmt.Println("Main commit");
 }
